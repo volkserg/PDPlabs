65fca44c7a57f15429b72d46cb16c2a000083501
ssvolkov <ssvolkov@ssvolkov.isa.ru>
2019-09-17 15:27:46 +0300

    generate lists of numbers

--- lab2/lab2_1.cpp
+++ lab2/lab2_1.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-unsigned long long int * random1(int n, int seed){
+unsigned long long int * random1(int n, unsigned long long seed){
     unsigned long long int * res = new unsigned long long int[n];
     unsigned long long int m = 18446744073709551615;
     unsigned long long int a = 6364136223846793005;
@@ -17,7 +17,7 @@ unsigned long long int * random1(int n, int seed){
     return res;
 }
 
-unsigned long long int * random2(int n, int seed){
+unsigned long long int * random2(int n, unsigned long long seed){
     unsigned long long int * res = new unsigned long long int[n];
     unsigned long long int a = 2685821657736338717;
     unsigned long long int x = seed;
@@ -36,23 +36,34 @@ int main(int argc, char *argv[])
 {
     int n = atoi(argv[1]);
 
+    ofstream res1, res2;
+
     ifstream devrand;
     devrand.open("/dev/random", ios::in | ios::binary);
     unsigned long long int seed;
     const auto bytes_num = sizeof seed;
-    devrand.read((char *) &seed,bytes_num);
+    devrand.read((char *) &seed, bytes_num);
     devrand.close();
     
     unsigned long long int *r1 = random1(n,seed);
     unsigned long long int *r2 = random2(n,seed);
     cout<<"-----------------FIRST RANDOM--------------------"<<endl;
+
+    res1.open("random1_" + to_string(n) + ".txt");
     for(int i=0; i<n;i++){
+        res1 << r1[i] << endl;
         cout<<r1[i]<<endl;
     }
+    res1.close();
+
     cout<<"-----------------SECOND RANDOM--------------------"<<endl;
+
+    res2.open("random2_" + to_string(n) + ".txt");
     for(int i=0; i<n;i++){
+        res2 << r2[i] << endl;;
         cout<<r2[i]<<endl;
     }
+    res2.close();
 
     return 0;
 }
