3c5fcba5b734d32a3ba7af21fcc2a1f237ea6c9b
ssvolkov <ssvolkov@ssvolkov.isa.ru>
2019-09-17 16:32:46 +0300

    python graph builder

---
+++ lab2/graph.py
@@ -0,0 +1,25 @@
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+import sys
+
+def histogram(numbers): 
+	data = numbers 
+	plt.hist(data, bins=1000, density=True, histtype='step')
+	plt.title('Распределение для '+str(len(data))+' чисел')
+	plt.xlabel('Значение')
+	plt.ylabel('Количество')
+
+	plt.show() 
+
+
+def main():
+    filename = sys.argv[1]
+    mylist = []
+    with open(filename,'r') as file:
+        for row in file:
+            mylist.append(int(row))
+    histogram(mylist)
+
+if __name__ == "__main__":
+    main()
